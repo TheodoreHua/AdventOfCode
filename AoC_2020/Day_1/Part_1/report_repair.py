4b12451e0327b2824ee60481f5f1e875254fc981
Theodore Hua <theodorehuadev@gmail.com>
2020-12-01 09:16:15 -0500

    Day 1

---
+++ AoC_2020/Day_1/Part_1/report_repair.py
@@ -0,0 +1,18 @@
+def sum_check(target:int, values:list):
+    compare_list = values[:]
+    for i in values:
+        for j in compare_list:
+            if i + j == target:
+                print("{0} + {1} = {2}\n{0} * {1} = {3}".format(i, j, i + j, i * j))
+                return
+
+
+with open("input.txt", "r") as f:
+    report = f.readlines()
+    try:
+        report = [int(r.strip()) for r in report]
+    except ValueError:
+        print("Invalid Value in Report")
+        exit(1)
+
+sum_check(2020, report)
---
+++ AoC_2020/Day_1/Part_2/report_repair.py
@@ -0,0 +1,20 @@
+def sum_check(target:int, values:list):
+    compare_list = values[:]
+    compare_list_2 = values[:]
+    for i in values:
+        for j in compare_list:
+            for k in compare_list_2:
+                if i + j + k == target:
+                    print("{0} + {1} + {2} = {3}\n{0} * {1} * {2} = {4}".format(i, j, k, i + j + k, i * j * k))
+                    return
+
+
+with open("input.txt", "r") as f:
+    report = f.readlines()
+    try:
+        report = [int(r.strip()) for r in report]
+    except ValueError:
+        print("Invalid Value in Report")
+        exit(1)
+
+sum_check(2020, report)
