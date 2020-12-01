ef2a9e833fa9fee130c4740891a1f03a4a0cd981
Theodore Hua <theodorehuadev@gmail.com>
2020-12-01 10:10:35 -0500

    Add LICENSE

--- AoC_2020/Day_1/Part_2/report_repair.py
+++ AoC_2020/Day_1/Part_2/report_repair.py
@@ -1,3 +1,9 @@
+"""
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+
 def sum_check(target:int, values:list):
     compare_list = values[:]
     compare_list_2 = values[:]
