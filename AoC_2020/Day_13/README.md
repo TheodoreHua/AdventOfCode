0b68b35611f08119a40d8bfb1ec907cf1a178a00
Theodore Hua <theodorehuadev@gmail.com>
2020-12-13 13:33:46 -0500

    Semi-Slow Part 2

---
+++ AoC_2020/Day_13/README.md
@@ -0,0 +1,3 @@
+Solution for Part 2 is quite slow (took around 18 minutes for me) due to a semi-brute force approach. It's basically 
+brute force but instead of adding 1 each time, it adds the period between the first and most recent 
+(correct order/minute) bus.
--- AoC_2020/Day_13/part2.py
+++ AoC_2020/Day_13/part2.py
@@ -8,9 +8,12 @@ from math import ceil
 
 def get_subsequent_timestamp(ids):
     timestamp = 1
+    period_incre = 1
     times = []
+    last_period = {}
+    bus_periods = {}
     while True:
-        print(timestamp)
+        print(timestamp, len(str(timestamp)), period_incre)
         for id in ids:
             if id != "x":
                 id = int(id)
@@ -18,29 +21,33 @@ def get_subsequent_timestamp(ids):
             else:
                 times.append("x")
         if times[0] != timestamp:
-            timestamp += 1
+            timestamp += period_incre
             times = []
             continue
-        incre_count = 1
-        rtrn = False
+        incre_count = 0
+        bus_count = 0
         for index, time in enumerate(times):
-            if index == 0:
-                continue
             if time == "x":
                 incre_count += 1
                 continue
+            else:
+                bus_count += 1
             if time - timestamp != incre_count:
                 break
             if index == len(times) - 1:
-                rtrn = True
+                return timestamp
+            else:
+                if bus_count not in last_period.keys():
+                    last_period[bus_count] = timestamp
+                elif bus_count not in bus_periods.keys():
+                    bus_periods[bus_count] = timestamp - last_period[bus_count]
+                    period_incre = bus_periods[bus_count]
             incre_count += 1
-        if rtrn:
-            return timestamp
-        timestamp += 1
+        timestamp += period_incre
         times = []
 
 
-with open("data/input.txt", "r") as f:
+with open("data/test_input.txt", "r") as f:
     data = f.readlines()
 
-print(get_subsequent_timestamp(data[1].split(",")))
+print("---\n", get_subsequent_timestamp(data[1].split(",")), "\n---")
