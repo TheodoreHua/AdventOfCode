1107b780dab53129f580e7b888eb511ae00e1706
Theodore Hua <theodorehuadev@gmail.com>
2020-12-12 23:32:49 -0500

    Remove commented debug code and remove unused import

--- AoC_2020/Day_12/part2.py
+++ AoC_2020/Day_12/part2.py
@@ -4,7 +4,7 @@
 #   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # ------------------------------------------------------------------------------
 
-from math import floor, sin, cos, sqrt, atan, asin, radians
+from math import floor, sin, cos, sqrt, asin, radians
 
 DEGREES = {-0: "E", -90: "S", -180: "W", -270: "N",
            0: "E", 90: "N", 180: "W", 270: "S"}
@@ -45,10 +45,8 @@ def get_manhattan(instructions):
             rotation -= num
             if rotation <= -360:
                 rotation = rotation - 360 * floor(rotation / 360)
-            # print(waypoint_coordinates)
             rotated = rotate(ship_coordinates, waypoint_coordinates, -num)
             waypoint_coordinates = [round(rotated[0]), round(rotated[1])]
-            # print(waypoint_coordinates)
         elif instruc == "F":
             x_diff = waypoint_coordinates[0] - ship_coordinates[0]
             y_diff = waypoint_coordinates[1] - ship_coordinates[1]
@@ -59,7 +57,6 @@ def get_manhattan(instructions):
         print(instruction, "SX", ship_coordinates[0], "SY", ship_coordinates[1], "WX", waypoint_coordinates[0],
               "WY", waypoint_coordinates[1],"Rotation", rotation, ":", DEGREES[rotation], "SWDX",
               waypoint_coordinates[0] - ship_coordinates[0], "SWDY", waypoint_coordinates[1] - ship_coordinates[1])
-    # print(ship_coordinates)
     return abs(ship_coordinates[0]) + abs(ship_coordinates[1])
 
 
