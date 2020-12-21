1d0dca3969c7b1759489588a74337b6ae3568ff3
Theodore Hua <theodorehuadev@gmail.com>
2020-12-21 13:15:39 -0500

    Not Mine Day 20

--- AoC_2020/Day_20/part1.py
+++ AoC_2020/Day_20/part1.py
@@ -1,49 +1,84 @@
-# ------------------------------------------------------------------------------
-#  This Source Code Form is subject to the terms of the Mozilla Public
-#  License, v. 2.0. If a copy of the MPL was not distributed with this
-#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
-# ------------------------------------------------------------------------------
+import collections
+import sys
 
-from math import floor
 
-class Tile:
-    def __init__(self, id, rows):
-        self.id = id
-        self.row_count = len(rows)
-        self.column_count = len(rows[0])
-        self.grid = [list(row) for row in rows]
+def tile_edges(tile):
+    return [
+        tile[0],
+        tile[-1][::-1],
+        ''.join(row[-1] for row in tile),
+        ''.join(row[0] for row in tile[::-1]),
+    ]
 
-    def rotate(self):
-        """Rotate right 90°"""
-        rotated = [[""]*self.row_count for i in range(self.column_count)]
-        for row in range(self.row_count):
-            for column in range(self.column_count):
-                rotated[column][-row - 1] = self.grid[row][column]
-        self.row_count, self.column_count = self.column_count, self.row_count
-        self.grid = rotated
 
-    def flip(self, direction):
-        if direction == "horizontal":
-            for row in self.grid:
-                row.reverse()
-        elif direction == "vertical":
-            for i in range(floor(self.row_count/2)):
-                self.grid[i] = self.grid[-i-1]
-                self.grid[-i-1] = self.grid[i]
+def parse_tiles(f):
+    tiles = {}
+    for line in f:
+        tile_id = int(line.partition(':')[0].split()[-1])
+        tile = []
+        for line in f:
+            line = line.strip()
+            if not line:
+                break
+            tile.append(line)
+        tiles[tile_id] = tile
+    return tiles
+
+
+def count_edges(tiles):
+    edges = collections.Counter()
+    for tile in tiles.values():
+        edges.update(tile_edges(tile))
+    return edges
+
+
+def count_flipped_edges(tiles):
+    edges = collections.Counter()
+    for tile in tiles.values():
+        edges.update(tile_edges(tile[::-1]))
+    return edges
+
+
+with open("data/input.txt", "r") as f:
+    tiles = parse_tiles(f)
+
+if '-v' in sys.argv:
+    print(f"{len(tiles)} tiles")
+
+edges = count_edges(tiles)
+
+if '-v' in sys.argv:
+    print(f"{len(edges)} unique edges")
+
+edges += count_flipped_edges(tiles)
+
+if '-v' in sys.argv:
+    print(f"{len(edges)} unique edges, counting flipping")
+
+if '-v' in sys.argv:
+    for tile_id, tile in tiles.items():
+        print(f"Tile {tile_id} has these edges:")
+        for edge in tile_edges(tile):
+            print(f"  {edge} [{'*' * edges[edge]}]")
+
+corner = 1
+n_corners = 0
+for tile_id, tile in tiles.items():
+    # corner tiles have two shared edges and two unique edges
+    # other tiles have three or four shared edges
+    # (I am assuming at least a 3x3 tile grid!)
+    unique = 0
+    for edge in tile_edges(tile):
+        if edges[edge] == 1:
+            unique += 1
         else:
-            raise ValueError("Invalid Direction \"{}\"".format(direction))
+            # just making sure
+            assert edges[edge] == 2
+    if unique == 2:
+        n_corners += 1
+        corner *= tile_id
+        if '-v' in sys.argv:
+            print(f'Corner tile: {tile_id}')
 
-    def get_border(self, direction):
-        if direction == "top":
-            return self.grid[0]
-        elif direction == "right":
-            return [self.grid[i][0] for i in range(self.column_count)]
-        elif direction == "bottom":
-            return self.grid[-1]
-        elif direction == "left":
-            return [self.grid[i][0] for i in range(self.column_count)]
-        else:
-            raise ValueError("Invalid Direction \"{}\"".format(direction))
-
-    def __str__(self):
-        return "Tile {}\n{}".format(self.id, "\n".join(["".join(row) for row in self.grid])).replace("\n\n","\n")
+assert n_corners == 4
+print(corner)
