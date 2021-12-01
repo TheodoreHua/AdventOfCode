9658c02e2b6d18c4ab7a49c292a319ab238d6184
Theodore Hua <theodorehuadev@gmail.com>
2021-12-01 08:42:17 -0500

    Finish day 1 part 1; rewrite runner code

---
+++ AoC_2021/Day_01/part1.py
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ------------------------------------------------------------------------------
+
+def main(d:list, bar):
+    past = float('inf')
+    mapped = []
+    for i in d:
+        i = int(i)
+        mapped.append(i > past)
+        past = i
+        bar()
+
+    return sum(mapped)
--- AoC_2021/runner.py
+++ AoC_2021/runner.py
@@ -4,6 +4,7 @@
 #   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # ------------------------------------------------------------------------------
 
+import sys
 from datetime import timedelta
 from time import time
 from typing import Callable
@@ -19,28 +20,44 @@ def example_func(d, bar, target):
         bar()
     return "No result found"
 
-def run_aoc(func:Callable, test_runner:list=None, **kwargs):
+def run_aoc(func:Callable, input_path:str, test_runner:list=None, *args, **kwargs):
     """Default puzzle function runner code for AoC
 
     :param func: Function to run that takes arguments d (data) and bar (progress bar), expected to return final value
+    :param input_path: Path to the input file to run from
     :param test_runner: Whether or not the function is being run as a test
     """
     start = time()
 
     if test_runner is None:
-        with open("data/input.txt", "r") as f:
+        with open(input_path, "r") as f:
             d = [l.strip() for l in f.readlines()]
     else:
         d = test_runner
 
     with alive_bar(force_tty=True, unknown='stars') as bar:
-        r = func(d, bar, **kwargs)
+        r = func(d, bar, *args, **kwargs)
     print("Program successfully finished in {}, return value is '{}'".format(timedelta(seconds=time() - start), r))
 
 
 if __name__ == "__main__":
-    # Basic test if file is run by itself
-    from time import sleep
-    from random import randint
+    if len(sys.argv) >= 3:
+        try:
+            day = int(sys.argv[1])
+            part = int(sys.argv[2])
+            args = sys.argv[3:] if len(sys.argv) > 3 else []
+        except ValueError:
+            print("Day and part must be integers")
+            sys.exit(-1)
+        if not 25 >= day >= 1 or part not in (1, 2):
+            print("Invalid day or part value")
+            sys.exit(-1)
+        directory = "Day_{:02}".format(day)
+        module = __import__("{}.part{}".format(directory, part), fromlist=['main'])
+        run_aoc(getattr(module, 'main'), "{}/data/input.txt".format(directory, day), test_runner=None, *args)
+    else:
+        # Basic test if file is run by itself
+        from time import sleep
+        from random import randint
 
-    run_aoc(example_func, test_runner=[i for i in range(1,1000)], target=randint(1,1000))
+        run_aoc(example_func, None, test_runner=[i for i in range(1,1000)], target=randint(1,1000))
