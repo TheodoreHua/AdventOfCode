3e4e4277b2ef943e31584f8bb190542ec631e78b
Theodore Hua <theodorehuadev@gmail.com>
2021-12-01 10:50:51 -0500

    add comments

--- AoC_2021/Day_01/part1.py
+++ AoC_2021/Day_01/part1.py
@@ -5,12 +5,14 @@
 # ------------------------------------------------------------------------------
 
 def main(d: list, bar):
+    # Initiate default values, past is inf to make sure the first comparison is False
     past = float('inf')
     mapped = []
+    # Loop through each value and compare with the previous, add bool value to mapped list
     for i in d:
         i = int(i)
         mapped.append(i > past)
         past = i
         bar()
-
+    # Return number of Trues in bool list (True is 1, False is 0 for sum)
     return sum(mapped)
--- AoC_2021/Day_01/part2.py
+++ AoC_2021/Day_01/part2.py
@@ -5,15 +5,20 @@
 # ------------------------------------------------------------------------------
 
 def main(d: list, bar):
+    # Convert values in data to int and initiate default values
     converted_list = [int(i) for i in d]
     index = 0
     mapped = []
     past = float('inf')
     while index <= len(d):
+        # Get the sum of the current index and the next 2
         s = sum(converted_list[index:index+3])
+        # Check whether the current sum is larger than the past one
         mapped.append(s > past)
+        # Update loop vars
         past = s
         index += 1
+        # Increment alive-progress
         bar()
 
     return sum(mapped)
