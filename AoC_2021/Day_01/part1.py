518ba1de527701175e1d0389cd8f7735dce47b49
Theodore Hua <theodorehuadev@gmail.com>
2021-12-01 09:10:20 -0500

    Reformat part1

--- AoC_2021/Day_01/part1.py
+++ AoC_2021/Day_01/part1.py
@@ -4,7 +4,7 @@
 #   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # ------------------------------------------------------------------------------
 
-def main(d:list, bar):
+def main(d: list, bar):
     past = float('inf')
     mapped = []
     for i in d:
