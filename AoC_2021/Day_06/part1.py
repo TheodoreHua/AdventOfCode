e673ee8c26671056c9989f36545d222079c72c8d
Theodore Hua <theodorehuadev@gmail.com>
2021-12-06 11:22:35 -0500

    day 6, uploaded early so I can run it on a server

---
+++ AoC_2021/Day_06/part1.py
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------------------------
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ------------------------------------------------------------------------------
+
+from AoC_2021.Day_06.commons import lanternfish_simulation
+
+def main(d:list, bar):
+    return lanternfish_simulation(d, bar, 80)
