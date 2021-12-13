08e404e2bcc5a4f82229e5f5176a92b8705d55fa
Theodore Hua <theodorehuadev@gmail.com>
2021-12-13 10:17:13 -0500

    day 11

---
+++ AoC_2021/Day_11/commons.py
@@ -0,0 +1,52 @@
+# ------------------------------------------------------------------------------
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ------------------------------------------------------------------------------
+
+class Octopus:
+    def __init__(self, energy):
+        self.energy = energy
+        self.flashed = False
+
+    def reset(self):
+        self.flashed = False
+
+    def flash(self):
+        self.flashed = True
+
+    def is_flashed(self):
+        return self.flashed
+
+    def deplete(self):
+        if self.flashed:
+            self.energy = 0
+
+    def increment(self):
+        self.energy += 1
+
+    def should_flash(self):
+        return self.energy > 9 and not self.flashed
+
+
+def flash_adjacent(octopuses, x, y):
+    # if done is None:
+    #     done = []
+    # done.append((x, y))
+    adjacent = [
+        (x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1),
+        (x - 1, y - 1), (x - 1, y + 1), (x + 1, y + 1), (x + 1, y - 1)
+    ]
+    for cx, cy in adjacent:
+        if cx < 0 or cy < 0:
+            continue
+        try:
+            if not octopuses[cy][cx].is_flashed():
+                # done.append((cx, cy))
+                o = octopuses[cy][cx]
+                o.increment()
+                if o.should_flash():
+                    o.flash()
+                    flash_adjacent(octopuses, cx, cy)
+        except IndexError:
+            continue
---
+++ AoC_2021/Day_11/part1.py
@@ -0,0 +1,32 @@
+# ------------------------------------------------------------------------------
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ------------------------------------------------------------------------------
+
+from AoC_2021.Day_11.commons import *
+
+
+def main(d:list, bar):
+    octopuses = [[Octopus(int(j)) for j in i] for i in d]
+    mh, mw = len(octopuses), len(octopuses[0])
+    flashes = 0
+    for _ in range(100):
+        for l in octopuses:
+            for octopus in l:
+                octopus.increment()
+        for y in range(mh):
+            for x in range(mw):
+                o = octopuses[y][x]
+                if o.should_flash():
+                    o.flash()
+                    flash_adjacent(octopuses, x, y)
+        for l in octopuses:
+            for octopus in l:
+                if octopus.is_flashed():
+                    octopus.deplete()
+                    octopus.reset()
+                    flashes += 1
+        bar()
+
+    return flashes
