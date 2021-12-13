57e1a60afe4092b962091a72e8a57bea344b0239
Theodore Hua <theodorehuadev@gmail.com>
2021-12-13 09:37:23 -0500

    day 10

---
+++ AoC_2021/Day_10/part1.py
@@ -0,0 +1,36 @@
+# ------------------------------------------------------------------------------
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ------------------------------------------------------------------------------
+
+m = {
+    ')':'(',
+    ']':'[',
+    '}':'{',
+    '>':'<'
+}
+c = {
+    ')':3,
+    ']':57,
+    '}':1197,
+    '>':25137
+}
+
+def main(d:list, bar):
+    error_score = 0
+    for line in d:
+        characters = []
+        for char in line:
+            if char in m.values():
+                characters.append(char)
+            else:
+                o = m[char]
+                if o != characters[-1]:
+                    error_score += c[char]
+                    break
+                else:
+                    del characters[-1]
+        bar()
+
+    return error_score
---
+++ AoC_2021/Day_10/part2.py
@@ -0,0 +1,48 @@
+# ------------------------------------------------------------------------------
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ------------------------------------------------------------------------------
+
+from statistics import median
+
+m = {
+    ')':'(',
+    ']':'[',
+    '}':'{',
+    '>':'<'
+}
+c = {
+    ')':1,
+    ']':2,
+    '}':3,
+    '>':4
+}
+
+def get_by_value(dc, value):
+    for k,v in dc.items():
+        if v == value:
+            return k
+
+def main(d:list, bar):
+    error_scores = []
+    for line in d:
+        characters = []
+        co = True
+        for char in line:
+            if char in m.values():
+                characters.append(char)
+            else:
+                if m[char] != characters[-1]:
+                    co = False
+                    break
+                else:
+                    del characters[-1]
+        if co:
+            error_scores.append(0)
+            for opening in reversed(characters):
+                error_scores[-1] *= 5
+                error_scores[-1] += c[get_by_value(m, opening)]
+        bar()
+
+    return median(error_scores)
