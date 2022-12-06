6d65de46eea912fa511e595ac09b553274bc4a55
Theodore Hua <theodorehuadev@gmail.com>
2022-12-06 00:13:37 -0500

    Day 6

---
+++ AoC_2022/Day_06/part1.py
@@ -0,0 +1,14 @@
+# ------------------------------------------------------------------------------
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ------------------------------------------------------------------------------
+
+def main(d: list, bar):
+    for i in range(0, len(d[0])):
+        if len(set(d[0][i: i+4])) == 4:
+            return i + 4
+        bar()
+
+def oneliner(d: list, bar):
+    return [i for i in range(0, len(d[0])) if len(set(d[0][i: i+4])) == 4][0] + 4
---
+++ AoC_2022/Day_06/part2.py
@@ -0,0 +1,14 @@
+# ------------------------------------------------------------------------------
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ------------------------------------------------------------------------------
+
+def main(d: list, bar):
+    for i in range(0, len(d[0])):
+        if len(set(d[0][i: i+14])) == 14:
+            return i + 14
+        bar()
+
+def oneliner(d: list, bar):
+    return [i for i in range(0, len(d[0])) if len(set(d[0][i: i+14])) == 14][0] + 14
