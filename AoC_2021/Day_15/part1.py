b186b2803fef6dbbe20566adb8f2a48a740b8b40
Theodore Hua <theodorehuadev@gmail.com>
2022-12-13 11:01:41 -0500

    2021 Day 15

--- AoC_2021/Day_15/part1.py
+++ AoC_2021/Day_15/part1.py
@@ -4,5 +4,25 @@
 #   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # ------------------------------------------------------------------------------
 
+import networkx
+import numpy as np
+
+
 def main(d: list, bar):
-    pass
+    grid = np.zeros((len(d[0]), len(d)), dtype=int)
+    bar.text("Parsing input")
+    for y, row in enumerate(d):
+        for x, value in enumerate(row):
+            grid[x][y] = int(value)
+
+    bar.text("graphing grid")
+    G = networkx.grid_2d_graph(len(grid), len(grid[0]), create_using=networkx.DiGraph)
+    bar.text("Weighing edges")
+    for u, v in G.edges:
+        G[u][v]['weight'] = grid[v[0]][v[1]]
+        bar()
+
+    bar.text("Finding shortest path")
+    path = networkx.shortest_path(G, (0, 0), (len(d[0]) - 1, len(d) - 1), weight='weight')
+    bar.text("Calculating path cost")
+    return sum([grid[x, y] for x, y in path[1:]])
--- AoC_2021/Day_15/part2.py
+++ AoC_2021/Day_15/part2.py
@@ -4,5 +4,34 @@
 #   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # ------------------------------------------------------------------------------
 
+import networkx
+import numpy as np
+from itertools import product
+
+
 def main(d: list, bar):
-    pass
+    grid_segment = np.zeros((len(d[0]), len(d)), dtype=int)
+    grid = np.zeros((len(d[0]) * 5, len(d) * 5), dtype=int)
+    bar.text("Parsing input")
+    for y, row in enumerate(d):
+        for x, value in enumerate(row):
+            grid_segment[x][y] = int(value)
+            bar()
+    bar.text("Building grid")
+    for row, col in product(range(len(grid_segment[0]) * 5), range(len(grid_segment) * 5)):
+        grid[row][col] = grid_segment[row % len(grid_segment[0])][col % len(grid_segment)] + (row // len(grid_segment[0])) + (col // len(grid_segment))
+        bar()
+    bar.text("Checking grid")
+    grid[grid > 9] -= 9
+
+    bar.text("Graphing grid")
+    G = networkx.grid_2d_graph(len(grid), len(grid[0]), create_using=networkx.DiGraph)
+    bar.text("Weighing edges")
+    for u, v in G.edges:
+        G[u][v]['weight'] = grid[v[0]][v[1]]
+        bar()
+
+    bar.text("Finding shortest path")
+    path = networkx.shortest_path(G, (0, 0), (grid.shape[0] - 1, grid.shape[1] - 1), weight='weight')
+    bar.text("Calculating path cost")
+    return sum([grid[x, y] for x, y in path[1:]])
