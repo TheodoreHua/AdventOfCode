cf4ab49181fdda0a05113c4d2692221ded9fc029
Theodore Hua <contact@theodorehua.dev>
2023-12-09 00:45:59 -0500

    Day 9

---
+++ AoC_2023/Day_09/part2.py
@@ -0,0 +1,27 @@
+# ------------------------------------------------------------------------------
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ------------------------------------------------------------------------------
+
+from re import compile
+
+DIGIT_REGEX = compile(r"-?\d+")  # THERE WERE NEGATIVES
+
+def extrapolate(l):
+    diffs = []
+    for i in range(1, len(l)):
+        diffs.append(l[i] - l[i-1])
+    if any(diffs):  # not all 0s yet
+        backwards = extrapolate(diffs)
+        backwards.insert(0, diffs[0] - backwards[0])
+        return backwards
+    else:
+        return [0]
+
+def extrapolate_wrapper(l):
+    return l[0] - extrapolate(l)[0]
+
+def main(d: list, bar):
+    history = [list(map(int, DIGIT_REGEX.findall(i))) for i in d]
+    return sum(list(map(extrapolate_wrapper, history)))
