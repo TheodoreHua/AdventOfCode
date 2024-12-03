3608a0fd5f93c62b24634fd34c6c11614f665eed
Theodore Hua <contact@theodorehua.dev>
2024-12-03 00:15:36 -0500

    Day 3

---
+++ AoC_2024/Day_03/part1.py
@@ -0,0 +1,19 @@
+# ------------------------------------------------------------------------------
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ------------------------------------------------------------------------------
+
+from re import findall
+
+
+def main(d: str, bar):
+    s = 0
+    for i, j in findall(r"mul\((\d+),(\d+)\)", d):
+        s += int(i) * int(j)
+        bar()
+    return s
+
+
+def oneliner(d: str, bar):
+    return sum(int(i) * int(j) for i, j in findall(r"mul\((\d+),(\d+)\)", d))
---
+++ AoC_2024/Day_03/part2.py
@@ -0,0 +1,21 @@
+# ------------------------------------------------------------------------------
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# ------------------------------------------------------------------------------
+
+from re import findall
+
+
+def main(d: str, bar):
+    s = 0
+    do = True
+    for i, j, k in findall(r"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))", d):
+        if i == "do()":
+            do = True
+        elif i == "don't()":
+            do = False
+        elif do:
+            s += int(j) * int(k)
+        bar()
+    return s
